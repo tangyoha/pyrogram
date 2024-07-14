f13d6126999177aef611a38290f132f4eb902454
tangyoha <tangyoha@outlook.com>
2024-07-14 17:36:06 +0800

    fix: update id

--- pyrogram/methods/messages/inline_session.py
+++ pyrogram/methods/messages/inline_session.py
@@ -31,15 +31,14 @@ async def get_session(client: "pyrogram.Client", dc_id: int):
         if client.media_sessions.get(dc_id):
             return client.media_sessions[dc_id]
 
-        auth_key = (
-            await Auth(client, dc_id, await client.storage.test_mode()).create()
-            if dc_id != await client.storage.dc_id()
-            else await client.storage.auth_key())
-
         session = client.media_sessions[dc_id] = Session(
-                        client, dc_id, auth_key, await client.storage.test_mode(), is_media=True)
+            client, dc_id,
+            await Auth(client, dc_id, await client.storage.test_mode()).create(),
+            await client.storage.test_mode(), is_media=True
+        )
 
         await session.start()
+
         for _ in range(3):
             exported_auth = await client.invoke(
                 raw.functions.auth.ExportAuthorization(
